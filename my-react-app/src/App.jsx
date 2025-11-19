c1c836d6353fd1965d182d9ae6d187c85b89a609
30237507 <marena.matavao@ucalgary.ca>
2025-11-19 16:00:58 -0700

    Created the sparks bundle inventory check

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import ETB from './components/etb/ETB.jsx'
 import Booster from './components/Booster/Booster.jsx'
 import Bundle from './components/Bundle/Bundle.jsx'
 import SparksETB from './components/SparksETB/SparksETB.jsx'
+import SparksBundle from './components/SparksBundle/SparksBundle.jsx'
 function App() {
   return(
     <>
@@ -9,6 +10,7 @@ function App() {
     <Booster></Booster>
     <Bundle></Bundle>
     <SparksETB></SparksETB>
+    <SparksBundle></SparksBundle>
     </>
   )
 }
---
+++ my-react-app/src/components/SparksBundle/SparksBundle.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./SparksBundle.css"
+import SparkBundle from './SparksBundle.png'
+
+function SparksBundle(){
+    const [inventorySparkBundle, setInventorySparkBundle] = useState(0);
+
+    // Sparks Bundle
+    const increaseSparkBundle = () => {
+        setInventorySparkBundle(inventorySparkBundle + 1);
+    };
+
+    const decreaseSparkBundle = () => {
+        if(inventorySparkBundle > 0){
+            setInventorySparkBundle(inventorySparkBundle - 1);
+        }; // Error message here
+    };
+
+    return(
+        <div className = "card-five">
+            <img className = "card-image" src = {SparkBundle} alt = "Surging Sparks Booster Bundle"></img>
+            <h2 className = "card-title">Surging Sparks Booster Bundle</h2>
+            <h3 className = "card-inventory">Inventory Count: {inventorySparkBundle} </h3>
+
+            <button className = "card-add" onClick={increaseSparkBundle}>Add</button>
+            <button className = "card-remove" onClick={decreaseSparkBundle}>Remove</button>
+        </div>
+    )
+
+}
+
+export default SparksBundle
